6f53b80cd9ae3a2ef675a13c051398bee0eb1d5d
Vildan Softic <zewa666@gmail.com>
2018-10-13 11:17:42 +0200

    update tests with scenarios

--- dogfood/tests/calc.spec.js
+++ dogfood/tests/calc.spec.js
@@ -1,47 +1,48 @@
 import expect from "expect";
 
 const { sum, asyncSum } = require("../src/calc");
-const { test } = require("../../index");
+const { test, scenario } = require("../../index");
 
-
-test(sum, "it should add two numbers")
-  .arrange({
-    n1: 1,
-    n2: 2
-  })
-  .act((sut, ctx) => {
-    const { n1, n2 } = ctx; 
-    
-    return sut(n1, n2);
-  })
-  .assert(
-    (result) => { expect(result).toEqual(3); }
-  );
-
-test(sum, "it should add multiple numbers")
-  .arrange({
-    numbers: [1, 2, 3, 4, 5]
-  })
-  .act((sut, ctx) => {
-    const { numbers } = ctx; 
-
-    return sut(...numbers);
-  })
-  .assert(
-    (result) => expect(result).toBe(15)
-  );
-
-test(asyncSum, "it should add two async values")
-  .arrange({
-    valueFromServerA: Promise.resolve(4),
-    valueFromServerB: new Promise((resolve) => {
-      setTimeout(() => resolve(38), 1);
+scenario("the sum function", () => {
+  test(sum, "it should add two numbers")
+    .arrange({
+      n1: 1,
+      n2: 2
     })
-  })
-  .act((sut, ctx) => {
-    const { valueFromServerA, valueFromServerB} = ctx;
-    return sut(valueFromServerA, valueFromServerB);
-  })
-  .assert(
-    async (result) => expect(await result).toBe(42)
-  );
+    .act((sut, ctx) => {
+      const { n1, n2 } = ctx;
+
+      return sut(n1, n2);
+    })
+    .assert(
+      (result) => { expect(result).toEqual(3); }
+    );
+
+  test(sum, "it should add multiple numbers")
+    .arrange({
+      numbers: [1, 2, 3, 4, 5]
+    })
+    .act((sut, ctx) => {
+      const { numbers } = ctx;
+
+      return sut(...numbers);
+    })
+    .assert(
+      (result) => expect(result).toBe(15)
+    );
+
+  test(asyncSum, "it should add two async values")
+    .arrange({
+      valueFromServerA: Promise.resolve(4),
+      valueFromServerB: new Promise((resolve) => {
+        setTimeout(() => resolve(38), 1);
+      })
+    })
+    .act((sut, ctx) => {
+      const { valueFromServerA, valueFromServerB } = ctx;
+      return sut(valueFromServerA, valueFromServerB);
+    })
+    .assert(
+      async (result) => expect(await result).toBe(42)
+    );
+});
--- dogfood/tests/thread.calc.spec.js
+++ dogfood/tests/thread.calc.spec.js
@@ -1,23 +1,26 @@
 import { spawn } from "threads";
 
-const thread = spawn(function(input, done) {
+
+const thread = spawn(function (input, done) {
   const expect = require("expect");
   const { sum } = require(input.__dirname + "/../src/calc");
-  const { test } = require(input.__dirname + "/../../index");
+  const { test, scenario } = require(input.__dirname + "/../../index");
 
-  test(sum, "it should add two numbers")
-    .arrange({
-      n1: 1,
-      n2: 2
-    })
-    .act((sut, ctx) => {
-      const { n1, n2 } = ctx; 
-      
-      return sut(n1, n2);
-    })
-    .assert(
-      (result) => { expect(result).toEqual(3); }
-    ).then(done);
+  scenario("using sum in threads", () => {
+    test(sum, "it should add two numbers")
+      .arrange({
+        n1: 1,
+        n2: 2
+      })
+      .act((sut, ctx) => {
+        const { n1, n2 } = ctx;
+
+        return sut(n1, n2);
+      })
+      .assert(
+        (result) => { expect(result).toEqual(3); }
+      ).then(done);
+  });
 });
 
 thread.send({ __dirname }).on("message", () => setTimeout(() => thread.kill()));
